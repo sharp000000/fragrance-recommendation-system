bee4b9481c424e736cb5d75bee208ce6226b1f92
Haiane Madoian <haianemadoian@Haianes-MBP.lan>
2021-10-26 00:54:04 +0200

    parsing fixed due to changes in css classes

--- sephora/sephora parser.py
+++ sephora/sephora parser.py
@@ -1,39 +1,50 @@
 from bs4 import BeautifulSoup
 import selenium 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import json
 import pandas as pd
 import time
+import warnings 
+warnings.filterwarnings("ignore")
+
 
 base_url = 'https://www.sephora.com'
 url = 'https://www.sephora.com/shop/fragrances-for-women'
+css_class_pages = 'css-om7j74'
 
-driver = webdriver.Chrome('../chromedriver') 
-
-def getPages(url):
+def get_pages(url, css_class_pages):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
     parsed_html = BeautifulSoup(driver.page_source, 'html.parser')
-    return int(parsed_html.find('div', 'css-6su6fj').find_all('li')[-1].find('button', 'css-cx0up1 eanm77i0').text)
+    driver.quit()
+    return int(int(parsed_html.find('div', css_class_pages).text.split(' ')[2])/60) + 1 
 
+def collect_parfume_links(pages):
+    parfume_links = []
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
-def collectParfumeLinks(pages):
-    parfumeLinks = []
-    driver = webdriver.Chrome('../chromedriver') 
     for page in range(1, pages+1):
         driver.get('{0}?currentPage={1}'.format(url, page))
         y = 0
         # print(driver.execute_script('return document.body.scrollHeight;'))
         while y < driver.execute_script('return document.body.scrollHeight;'):
             driver.execute_script("window.scrollTo(0, "+str(y)+")")
-            y += 200
+            y += 100
+            time.sleep(1)
         parsed_html = BeautifulSoup(driver.page_source, 'html.parser')
         
-        for i in parsed_html.find_all('div', 'css-dkxsdo'):
+        for i in parsed_html.find_all('div', 'css-1322gsb'):
             for a in i.find_all('a', href=True):
-                parfumeLinks.append('{0}{1}'.format(base_url, a['href']))
+                parfume_links.append('{0}{1}'.format(base_url, a['href']))
 
-    return parfumeLinks
+    return parfume_links
 
-pages = getPages(url)
-collectParfumeLinks(pages)
+
+pages = get_pages(url, css_class_pages)
+parfume_links = collect_parfume_links(pages)
+
+with open('parfume_links.txt', 'w') as f:
+    for item in parfume_links:
+        f.write("%s\n" % item)
